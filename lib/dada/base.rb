289964e2cbf6a6a6e7404f7a911a681a891fa471
Timon Vonk <mail@timonv.nl>
2011-10-28 13:10:48 +0200

    Fixed dada trying to map an empty hash of args in constructor

--- lib/dada/base.rb
+++ lib/dada/base.rb
@@ -20,7 +20,7 @@ module Dada
     # It parses the hash, builds the objects and sets new to false
     def self.build_object(args)
       args.stringify_keys!
-      args = args[self.name.downcase]
+      args = args[self.name.downcase] || {}
       obj = self.new(args)
       obj.tap { |obj| obj.instance_variable_set('@notsaved',false) } # because I don't want a global writer
     end
